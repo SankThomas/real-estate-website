e6a9632004b6e6c941bfca91063dc85a1379ef7a
Thomas Sankara <thomassankara74@gmail.com>
2020-11-29 17:45:32 +0300

    data.js files

---
+++ data files/stats-data.js
@@ -0,0 +1,22 @@
+export const stats = [
+  {
+    id: 1,
+    number: 1682,
+    title: 'Homes for Sale'
+  },
+  {
+    id: 2,
+    number: 178,
+    title: 'Open Houses'
+  },
+  {
+    id: 3,
+    number: 2786,
+    title: 'Recently Sold'
+  },
+  {
+    id: 4,
+    number: 475,
+    title: 'Price Reduced'
+  }
+]
